eca16f267c9730c3522acab18cffd4c71c96b635
norma92 <norma.santella@gmail.com>
2023-09-06 12:12:49 +0200

    penpal entity

---
+++ penpals_list/src/typeorm/entities/PenPal.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class PenPal {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 30 })
+    nome: string;
+
+    @Column({ length: 30 })
+    cognome: string;
+
+    @Column({ nullable: true })
+    compleanno: Date;
+
+    @Column()
+    affinita: number;
+}

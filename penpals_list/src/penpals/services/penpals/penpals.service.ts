eb2a2b1f7679175ca49fa7422fcfb8775c3789df
norma92 <norma.santella@gmail.com>
2023-09-06 14:02:06 +0200

    wip service

--- penpals_list/src/penpals/services/penpals/penpals.service.ts
+++ penpals_list/src/penpals/services/penpals/penpals.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
+/**
+ * la classe contiene i metodi che verranno invocati dal controller
+ */
 @Injectable()
-export class PenpalsService {}
+export class PenpalsService {
+
+    findUser() {}
+    
+    findUsers() {}
+
+    createUser() {}
+
+    updateUser() {}
+
+    deleteUser() {}
+
+    deleteUsers() {}
+
+}
